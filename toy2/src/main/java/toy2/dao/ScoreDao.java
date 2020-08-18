83cf08aae9e1332a733129991e6b1fdf79351fdb
equeue <latancy486@naver.com>
2020-08-19 02:59:03 +0900

    add:quiz system

---
+++ toy2/src/main/java/toy2/dao/ScoreDao.java
@@ -0,0 +1,5 @@
+package toy2.dao;
+
+public class ScoreDao {
+
+}
---
+++ toy2/src/main/java/toy2/service/QuizService.java
@@ -0,0 +1,13 @@
+package toy2.service;
+
+import java.util.List;
+
+import toy2.dto.QuizDto;
+
+public interface QuizService {
+	
+	
+	
+	public List<QuizDto> getQuizzes();
+
+}
